b41a4b7fe3a15c989b87eb90c6f87c4a55b756e0
Neo Titebe <Neo.Titebe@ominsure.co.za>
2023-11-22 20:58:06 +0200

    Initial commit

---
+++ app/(dashboard)/_components/sidebar.tsx
@@ -0,0 +1,16 @@
+import { Logo } from "./logo";
+import { SidebarRoutes } from "./sidebar-routes";
+
+export const Sidebar = () => {
+  return (
+    <div className="h-full border-r flex flex-col overflow-auto bg-white shadow-sm">
+      <div className="p-6">
+        <Logo />
+      </div>
+
+      <div className="flex felx-col w-full">
+        <SidebarRoutes />
+      </div>
+    </div>
+  )
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import './globals.css';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import { ClerkProvider } from '@clerk/nextjs';
+import { ToastProvider } from '@/components/providers/toaster-provider';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,8 +17,12 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={inter.className}>
+          <ToastProvider />
+          {children}</body>
+      </html>
+    </ClerkProvider>
   )
 }
